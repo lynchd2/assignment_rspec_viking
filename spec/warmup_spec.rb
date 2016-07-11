1d53f92656f7960a7c78fbf678b7d38cfa8fcd92
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-11 14:10:28 -0400

    Finish triple size method, start call_methods(string) method

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -1,18 +1,30 @@
 require 'warmup'
 require 'spec_helper'
 
+RSpec.describe Warmup do 
 let(:shouter) { Warmup.new }
 
-RSpec.describe Warmup do 
-
-  describe "#gets_shout" do
-
-    allow(shouter).to receive(:gets).and_return("hello world")
-
+  describe "#gets_shout" do 
     it "capitalizes a string" do 
+
+      allow(shouter).to receive(:gets).and_return("hello world")
       expect(shouter.gets_shout).to eq("HELLO WORLD")
+
     end
 
+  end
+
+  describe "#triple_size" do
+    it "triples the size of the array" do
+      new_array = double("triple", size: 4)
+      expect(shouter.triple_size(new_array)).to eq(12)
+    end
+  end
+
+  describe "#call_methods(string)" do
+    it "calls upcase on string parameter" do
+      string = "string"
+      expect(:shouter).to recieve()
   end
 
 end
